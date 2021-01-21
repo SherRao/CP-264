3ccd889e2c226d01eb79cc18c3004588e23248a4
Nausher Rao <SherRao15@gmail.com>
2021-01-21 00:54:19 -0500

    Finished assignment 1

--- Assignment 1/quadratic.c
+++ Assignment 1/quadratic.c
@@ -9,47 +9,62 @@
 #include <stdio.h>
 #include <math.h>
 
+const float EPSILON = 1e-6;
+
 int main() {
-	double a;
-	double b;
-	double c;
-	while(a + b + c != 0) {
-		printf("Please enter the coefficients a,b,c\n");
-		int inputLength = scanf("%f,%f,%f", &a, &b, &c);
-		char temp;
-		do {
-			scanf("%c", &temp);
-	        if (temp == '\n')
-	        	break;
-	    } while (1);
-
-		if(inputLength == 3) {
-			if(a != 0) {
-				double d = pow(b, 2) - (4.0000000 * a * c) + 0.000005;
-				if(d == 0) {
-					double x = -b / (2.0000000 * a);
-					printf("The equation has two equal real roots\nx:%f\n", x);
-
-				} else if (d < 0) {
-					double real = -b / (2.0000000 * a);
-					double imag = sqrt( fabs(d) ) / (2.0000000 * a);
-					printf("The equation has two complex roots\nreal:%f\nimaginary:%f\n", real, imag);
-
-				} else {
-					double x1 = ((-b + sqrt(d)) / (2.0000000 * a));
-					double x2 = ((-b - sqrt(d)) / (2.0000000 * a));
-					printf("The equation has two distinct real roots\nx1:%f\nx2:%f\n", x1, x2);
-
-				}
-
+	float a;
+  	float b;
+  	float c;
+  	char temp;
+  
+	do {
+    	do {   
+      		printf("Please enter the coefficients a,b,c\n");
+      		int inn = scanf("%f,%f,%f", &a, &b, &c);
+      		if (inn != 3 ) {
+         		printf("input:Invalid input\n");
+	   		
 			} else
-				printf("input:not a quadratic equation\n");
+        		break;
+       
+      		do {
+        		scanf("%c", &temp);
+        		if (temp == '\n') 
+					break;
+      		
+			  } while (1);
+    	
+		} while (1);
 
-		} else
-			printf("input:Invalid input\n");
+		if(fabs(a) < EPSILON && fabs(b) < EPSILON && fabs(c) < EPSILON) {
+			printf("input:quit\n");
+			break;
+		
+		} else if (fabs(a) < EPSILON) {
+			printf("input:not a quadratic equation\n");
+		
+		} else { 
+			float d = pow(b, 2) - (4 * a * c);
+			if(fabs(d) == 0) {
+				double x = -b / (2 * a);
+				printf("The equation has two equal real roots\nx:%f\n", x);
 
-	}
+			} else if (d < 0) {
+				double real = -b / (2 * a);
+				double imag = sqrt( fabs(d) ) / (2 * a);
+				printf("The equation has two complex roots\nreal:%f\nimaginary:%f\n", real, imag);
 
-	return 0;
+			} else {
+				double x1 = ((-b + sqrt(d)) / (2 * a));
+				double x2 = ((-b - sqrt(d)) / (2 * a));
+				printf("The equation has two distinct real roots\nx1:%f\nx2:%f\n", x1, x2);
+				
+			}
+		
+		}
+
+	} while (1);
+  
+  return 0;
+
 }
-
